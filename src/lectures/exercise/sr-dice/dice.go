344b560607f7385fac06cfa7f5d14881fbed6b77
MiloValenzuela <valenzuelagarza@gmail.com>
2025-02-19 18:19:51 -0600

    Dice Roller project

--- src/lectures/exercise/sr-dice/dice.go
+++ src/lectures/exercise/sr-dice/dice.go
@@ -23,8 +23,35 @@
 
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
 
-func main() {
+	"golang.org/x/exp/rand"
+)
+
+func roll(sides int) int {
+	return rand.Intn(sides) + 1
 }
 
+func main() {
+	rand.Seed(time.Now().UnixNano())
+
+	dice, sides := 2, 12
+	rolls := 1
+
+	for r := 1; r <= rolls; r++ {
+		sum := 0
+
+		for d := 1; d <= dice; d++ {
+			rolled := roll(sides)
+			sum += rolled
+			fmt.Println("Roll #", r, ", Die", d)
+		}
+		fmt.Println("Total rolled", sum)
+		switch sum := sum; {
+		case sum == 2 && dice == 2:
+
+		}
+	}
+}
