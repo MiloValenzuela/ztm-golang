8cabe1568808afe8bd0bd85e624a89cab61e22fe
MiloValenzuela <valenzuelagarza@gmail.com>
2025-03-03 16:18:48 -0600

    exercise:Structures

--- src/lectures/exercise/structs/structs.go
+++ src/lectures/exercise/structs/structs.go
@@ -19,7 +19,50 @@ package main
 
 import "fmt"
 
-func main() {
-
+type Coordinate struct {
+	x, y int
 }
 
+//* Create a rectangle structure containing a length and width field
+type Rectangle struct {
+	a Coordinate // top left
+	b Coordinate // bottom right
+}
+
+func width(rect Rectangle) int {
+	return (rect.b.x - rect.a.x)
+}
+
+func length(rect Rectangle) int {
+	return (rect.a.y - rect.a.x)
+}
+
+//* Using functions, calculate the area and perimeter of a rectangle,
+func area(rect Rectangle) int {
+	return length(rect) * width(rect)
+}
+
+func perimeter(rect Rectangle) int {
+	return (width(rect) * 2) + (length(rect) * 2)
+}
+
+// - Print the results to the terminal
+func printInfo(rect Rectangle) {
+	fmt.Println("area is", area(rect))
+	fmt.Println("Perimeter is", perimeter((rect)))
+}
+
+//  - The functions must use the rectangle structure as the function parameter
+//* After performing the above requirements, double the size
+//  of the existing rectangle and repeat the calculations
+
+func main() {
+	rect := Rectangle{a: Coordinate{0, 7}, b: Coordinate{10, 0}}
+	//  - Print the new results to the terminal
+	printInfo(rect)
+
+	rect.a.y *= 2
+	rect.a.x *= 2
+
+	printInfo(rect)
+}
