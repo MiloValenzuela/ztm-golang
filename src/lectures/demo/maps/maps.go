6e71a453931e67abb311c7d9220cfde4070850da
MiloValenzuela <valenzuelagarza@gmail.com>
2025-03-12 16:21:23 -0600

    demo maps

--- src/lectures/demo/maps/maps.go
+++ src/lectures/demo/maps/maps.go
@@ -3,5 +3,27 @@ package main
 import "fmt"
 
 func main() {
-}
+	shoppingList := make(map[string]int)
 
+	shoppingList["eggs"] = 11
+	shoppingList["milk"] = 1
+	shoppingList["bread"] += 1
+
+	shoppingList["eggs"] += 1
+	fmt.Println(shoppingList)
+	fmt.Println("Milk deleted, new list:", shoppingList)
+
+	cereal, found := shoppingList["cereal"]
+	fmt.Println("Need cereal")
+	if !found {
+		fmt.Println("nope")
+	} else {
+		fmt.Println("yep", cereal, "boxes")
+	}
+
+	totalItems := 0
+	for _, amount := range shoppingList {
+		totalItems += amount
+	}
+	fmt.Println("There are", totalItems, "on the shopping list")
+}
