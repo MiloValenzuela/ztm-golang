b195fa6a428ddaccb5cab1c678d9a83d2c47d948
MiloValenzuela <valenzuelagarza@gmail.com>
2025-03-28 14:05:46 -0600

    iota

--- src/lectures/exercise/iota/iota.go
+++ src/lectures/exercise/iota/iota.go
@@ -16,13 +16,43 @@ package main
 
 import "fmt"
 
-func main() {
-	fmt.Println(add.calculate(2, 2)) // = 4
+// * Mathematical operations must be defined as constants using iota
+const (
+	Add = iota
+	Subtract
+	Multiply
+	Divide
+)
 
-	fmt.Println(sub.calculate(10, 3)) // = 7
+type Operation int
 
-	fmt.Println(mul.calculate(3, 3)) // = 9
-
-	fmt.Println(div.calculate(100, 2)) // = 50
+// * Write a receiver function that performs the mathematical operation
+func (op Operation) calculate(lhs, rhs int) int {
+	switch op {
+	case Add:
+		return lhs + rhs
+	case Subtract:
+		return lhs - rhs
+	case Multiply:
+		return lhs * rhs
+	case Divide:
+		return lhs / rhs
+	}
+	panic("unhandled operation")
 }
 
+//  on two operands
+//* Operations required:
+//  - Add, Subtract, Multiply, Divide
+//* The existing function calls in main() represent the API and cannot be changed
+
+func main() {
+	add := Operation(Add)
+	fmt.Println(add.calculate(2, 2)) // = 4
+	sub := Operation(Subtract)
+	fmt.Println(sub.calculate(10, 3)) // = 7
+	mul := Operation(Multiply)
+	fmt.Println(mul.calculate(3, 3)) // = 9
+	div := Operation(Divide)
+	fmt.Println(div.calculate(100, 2)) // = 50
+}
