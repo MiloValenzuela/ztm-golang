a0da27fff1796480c7393a51b860e8b02f91aa2b
MiloValenzuela <valenzuelagarza@gmail.com>
2025-04-21 23:38:50 -0600

    errors exercise

--- src/lectures/exercise/errors/errors.go
+++ src/lectures/exercise/errors/errors.go
@@ -16,3 +16,110 @@
 
 package timeparse
 
+import (
+	"errors"
+	"strconv"
+	"strings"
+	"testing"
+)
+
+type Time struct {
+	Hour   int
+	Minute int
+	Second int
+}
+
+func ParseTime(timeStr string) (Time, error) {
+	parts := strings.Split(timeStr, ":")
+	if len(parts) != 3 {
+		return Time{}, error.New("invalid time format: ,udy nr HH:MM:SS")
+	}
+
+	hour, err := strconv.Atoi(parts[0])
+	if err != nil {
+		return Time{}, error.New("Invalid hour: must be between 0 and 23")
+	}
+
+	minute, err := strconv.Atoi(parts[1])
+	if err != nil {
+		return Time{}, errors.New("invalid minute: " + parts[1])
+	}
+	if minute < 0 || minute > 59 {
+		return Time{}, errors.New("invalid minute: must be between 0 and 59")
+	}
+
+	second, err := strconv.Atoi(parts[2])
+	if err != nil {
+		return Time{}, errors.New("invalid second: " + parts[2])
+	}
+	if second < 0 || second > 59 {
+		return Time{}, errors.New("invalid second: must be between 0 and 59")
+	}
+
+	return Time{Hour: hour, Minute: minute, Second: second}, nil
+}
+
+func TestParseTime(t *testing.T) {
+	tests := []struct {
+		name    string
+		input   string
+		want    Time
+		wantErr bool
+		errMsg  string
+	}{
+		{
+			name:  "valid time",
+			input: "14:07:33",
+			want:  Time{Hour: 14, Minute: 7, Second: 33},
+		},
+		{
+			name:    "invalid format - too few parts",
+			input:   "14:07",
+			wantErr: true,
+			errMsg:  "invalid time format: must be HH:MM:SS",
+		},
+		{
+			name:    "invalid format - too many parts",
+			input:   "14:07:33:10",
+			wantErr: true,
+			errMsg:  "invalid time format: must be HH:MM:SS",
+		},
+		{
+			name:    "invalid hour - not a number",
+			input:   "AA:07:33",
+			wantErr: true,
+			errMsg:  "invalid hour: AA",
+		},
+		{
+			name:    "invalid minute - out of range",
+			input:   "14:65:33",
+			wantErr: true,
+			errMsg:  "invalid minute: must be between 0 and 59",
+		},
+		{
+			name:    "invalid second - negative",
+			input:   "14:07:-05",
+			wantErr: true,
+			errMsg:  "invalid second: -05",
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := ParseTime(tt.input)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("ParseTime(%q) error = %v, wantErr %v", tt.input, err, tt.wantErr)
+				return
+			}
+			if tt.wantErr {
+				if err.Error() != tt.errMsg {
+					t.Errorf("ParseTime(%q) error message = %q, want %q", tt.input, err.Error(), tt.errMsg)
+				}
+				return
+			}
+			if got != tt.want {
+				t.Errorf("ParseTime(%q) got = %v, want %v", tt.input, got, tt.want)
+			}
+		})
+	}
+}
